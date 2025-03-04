20b075999b20cb4399f259924e946928de190c5f
FazMeuVideo <fazmeuvideo1@gmail.com>
2025-03-04 20:10:10 -0300

    Adicionar escolha de conta no login com Google

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -37,7 +37,12 @@ export default NextAuth({
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "select_account"
+        }
+      }
     })
   ],
   pages: {
